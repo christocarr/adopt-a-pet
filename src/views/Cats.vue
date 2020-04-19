4c815c359b0a013969b384fdfd26794b1e5d6b5e
Chris Carr <christocarr@outlook.com>
2020-04-19 14:01:42 +0100

    Add h1 heading

--- src/views/Cats.vue
+++ src/views/Cats.vue
@@ -1,3 +1,17 @@
 <template>
-  <div>Cats</div>
+  <div>
+    <h1>Cats for Adoption</h1>
+    <b-table striped hover :items="cats" />
+  </div>
 </template>
+
+<script >
+import cats from '@/data/cats'
+export default {
+  data () {
+    return {
+      cats
+    }
+  }
+}
+</script>
