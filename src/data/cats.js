58e9b189a142ec95010855c6157cc26b2e3c34c4
Chris Carr <christocarr@outlook.com>
2020-04-20 09:57:55 +0100

    Add more cat  objects to cats array

--- src/data/cats.js
+++ src/data/cats.js
@@ -1,12 +1,42 @@
 export default [
   {
     name: 'Tibby',
-    breed: '',
-    gender: '',
+    breed: 'tabby',
+    gender: 'female',
     age: 2,
     color: 'Black',
     weight: 34,
-    location: '',
+    location: 'Wembley',
+    notes: ''
+  },
+  {
+    name: 'Kat',
+    breed: 'tabby',
+    gender: 'female',
+    age: 4,
+    color: 'ginger',
+    weight: 14,
+    location: 'Harrow',
+    notes: ''
+  },
+  {
+    name: 'Mouse',
+    breed: 'Brit blue',
+    gender: 'male',
+    age: 2,
+    color: 'grey',
+    weight: 10,
+    location: 'Kenton Town',
+    notes: ''
+  },
+  {
+    name: 'Marley',
+    breed: 'tabby',
+    gender: 'male',
+    age: 5,
+    color: 'black',
+    weight: 14,
+    location: 'Harrow',
     notes: ''
   }
 ]
